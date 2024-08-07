8b2fb1b176b0bb6da12de1228f3d357baa356fbf
fisiofaz <75371386+fisiofaz@users.noreply.github.com>
2024-08-07 05:57:08 -0400

    feito: Criando Model, View e Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   get "/articles", to: "articles#index"
 
   # Para mais detalhes da DSL disponível para esse arquivo, veja https://guides.rubyonrails.org/routing.html
